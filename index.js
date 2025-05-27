7a78d21dc81d83d6263ed9634cb676ac2212096e
haseeb525 <haseeb4444059@gmail.com>
2025-05-28 02:17:46 +0500

    botsssx

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use("/audio", express.static(path.join(__dirname)));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+
 
 // Password Hashing Helpers
 const hashPassword = async (password) => {
