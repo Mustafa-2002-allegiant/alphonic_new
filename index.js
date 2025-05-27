48dd5336d672502e11f38644ae11d90e86ceafa4
haseeb525 <haseeb4444059@gmail.com>
2025-05-28 02:50:06 +0500

    bot tmo

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use("/audio", express.static(path.join(__dirname)));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+
 
 // Password Hashing Helpers
 const hashPassword = async (password) => {
@@ -300,6 +301,6 @@ app.get("/bot-assignments", async (req, res) => {
 
 // ---- Start Server ----
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Unified server running on port ${PORT}`);
 });
